d6c862e01894fad37b8030bd58d2a5a6dfac4a3b
zidann77 <azeidan061@gmail.com>
2025-10-03 16:26:26 +0300

    edit location

--- PersonFolder/frmAddUpdatePerson.Designer.cs
+++ PersonFolder/frmAddUpdatePerson.Designer.cs
@@ -314,6 +314,7 @@
             // 
             // richTextBox1
             // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.Location = new System.Drawing.Point(129, 223);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(607, 78);
