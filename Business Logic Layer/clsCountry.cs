a3a99bd0fb400065451be344a7d68c313fd19042
zidann77 <azeidan061@gmail.com>
2026-01-15 01:20:39 +0300

    Business Logic Layer

---
+++ Business Logic Layer/clsCountry.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess_DVLD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic_DVLD
+{
+    public class clsCountry
+    {
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+
+        public clsCountry()
+        {
+            CountryID = -1;
+            CountryName = "";
+        }
+
+        private clsCountry(int ID, string Name)
+        {
+            CountryID = ID;
+            CountryName = Name;
+
+        }
+
+        public static clsCountry FindByID(int ID)
+        {
+
+            string countryName = "";
+
+            if (clsCountryData.getCountryInfoByID(ID, ref countryName))
+            {
+                return new clsCountry(ID, countryName);
+            }
+            else
+                return null;
+        }
+
+
+        public static clsCountry FindByName(string Name)
+        {
+
+           int IDNumber = -1;
+
+            if (clsCountryData.getCountryInfoByName(Name,ref IDNumber))
+            {
+                return new clsCountry(IDNumber, Name);
+            }
+            else
+                return null;
+        }
+
+
+        public static DataTable getAllCountries()
+        {
+            return clsCountryData.getAllCountry();
+        }
+
+
+    }
+}
---
+++ Business Logic Layer/clsTestType.cs
@@ -0,0 +1,104 @@
+﻿using DataAccess_DVLD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic_DVLD
+{
+    public class clsTestType
+    {
+
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+        public enum enTestType { VisionTest = 1, WrittenTest = 2, StreetTest = 3 };
+
+        public clsTestType.enTestType ID { set; get; }
+        public string Title { set; get; }
+        public string Description { set; get; }
+        public float Fees { set; get; }
+        public clsTestType()
+
+        {
+            this.ID = clsTestType.enTestType.VisionTest;
+            this.Title = "";
+            this.Description = "";
+            this.Fees = 0;
+            Mode = enMode.AddNew;
+
+        }
+
+        public clsTestType(clsTestType.enTestType ID, string TestTypeTitel, string Description, float TestTypeFees)
+
+        {
+            this.ID = ID;
+            this.Title = TestTypeTitel;
+            this.Description = Description;
+
+            this.Fees = TestTypeFees;
+            Mode = enMode.Update;
+
+        }
+
+        private bool _AddNewTestType()
+        {
+            //call DataAccess Layer 
+
+            this.ID = (clsTestType.enTestType)clsTestTypeData.AddNewTestType(this.Title, this.Description, this.Fees);
+            return (this.Title != "");
+        }
+
+        private bool _UpdateTestType()
+        {
+            //call DataAccess Layer 
+
+            return clsTestTypeData.UpdateTestType((int)this.ID, this.Title, this.Description, this.Fees);
+        }
+
+        public static clsTestType Find(clsTestType.enTestType TestTypeID)
+        {
+            string Title = "", Description = ""; float Fees = 0;
+
+            if (clsTestTypeData.GetTestTypeInfoByID((int)TestTypeID, ref Title, ref Description, ref Fees))
+
+                return new clsTestType(TestTypeID, Title, Description, Fees);
+            else
+                return null;
+
+        }
+
+        public static DataTable GetAllTestTypes()
+        {
+            return clsTestTypeData.GetAllTestTypes();
+
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewTestType())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return _UpdateTestType();
+
+            }
+
+            return false;
+        }
+
+    }
+}
